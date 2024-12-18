48c47b4b9ff8f0061d81db87024406673335b8f9
juzai6 <3157676586@qq.com>
2024-12-19 00:15:27 +0800

    添加项目文件。

---
+++ 新用户管理系统/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace 新用户管理系统
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.nudown_Age = new System.Windows.Forms.NumericUpDown();
+            this.rbtn_Sex2 = new System.Windows.Forms.RadioButton();
+            this.rbtn_Sex1 = new System.Windows.Forms.RadioButton();
+            this.text_Adress = new System.Windows.Forms.TextBox();
+            this.text_Company = new System.Windows.Forms.TextBox();
+            this.text_Telephone = new System.Windows.Forms.TextBox();
+            this.text_Name = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lv_Customer = new System.Windows.Forms.ListView();
+            this.编号 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.姓名 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.单位 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.性别 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.年龄 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.电话 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.地址 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btn_Del = new System.Windows.Forms.Button();
+            this.lbl_Note = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudown_Age)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Cancel);
+            this.groupBox1.Controls.Add(this.btn_Save);
+            this.groupBox1.Controls.Add(this.nudown_Age);
+            this.groupBox1.Controls.Add(this.rbtn_Sex2);
+            this.groupBox1.Controls.Add(this.rbtn_Sex1);
+            this.groupBox1.Controls.Add(this.text_Adress);
+            this.groupBox1.Controls.Add(this.text_Company);
+            this.groupBox1.Controls.Add(this.text_Telephone);
+            this.groupBox1.Controls.Add(this.text_Name);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lbl_status);
+            this.groupBox1.Location = new System.Drawing.Point(64, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(633, 216);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "编辑客户信息  状态：";
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(362, 160);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cancel.TabIndex = 15;
+            this.btn_Cancel.Text = "取消";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(111, 160);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(75, 23);
+            this.btn_Save.TabIndex = 14;
+            this.btn_Save.Text = "保存";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // nudown_Age
+            // 
+            this.nudown_Age.Location = new System.Drawing.Point(464, 63);
+            this.nudown_Age.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudown_Age.Name = "nudown_Age";
+            this.nudown_Age.Size = new System.Drawing.Size(120, 25);
+            this.nudown_Age.TabIndex = 13;
+            this.nudown_Age.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // rbtn_Sex2
+            // 
+            this.rbtn_Sex2.AutoSize = true;
+            this.rbtn_Sex2.Location = new System.Drawing.Point(238, 63);
+            this.rbtn_Sex2.Name = "rbtn_Sex2";
+            this.rbtn_Sex2.Size = new System.Drawing.Size(43, 19);
+            this.rbtn_Sex2.TabIndex = 12;
+            this.rbtn_Sex2.TabStop = true;
+            this.rbtn_Sex2.Text = "女";
+            this.rbtn_Sex2.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_Sex1
+            // 
+            this.rbtn_Sex1.AutoSize = true;
+            this.rbtn_Sex1.Location = new System.Drawing.Point(122, 61);
+            this.rbtn_Sex1.Name = "rbtn_Sex1";
+            this.rbtn_Sex1.Size = new System.Drawing.Size(43, 19);
+            this.rbtn_Sex1.TabIndex = 11;
+            this.rbtn_Sex1.TabStop = true;
+            this.rbtn_Sex1.Text = "男";
+            this.rbtn_Sex1.UseVisualStyleBackColor = true;
+            // 
+            // text_Adress
+            // 
+            this.text_Adress.Location = new System.Drawing.Point(464, 94);
+            this.text_Adress.Name = "text_Adress";
+            this.text_Adress.Size = new System.Drawing.Size(100, 25);
+            this.text_Adress.TabIndex = 10;
+            // 
+            // text_Company
+            // 
+            this.text_Company.Location = new System.Drawing.Point(464, 24);
+            this.text_Company.Name = "text_Company";
+            this.text_Company.Size = new System.Drawing.Size(100, 25);
+            this.text_Company.TabIndex = 9;
+            // 
+            // text_Telephone
+            // 
+            this.text_Telephone.Location = new System.Drawing.Point(122, 104);
+            this.text_Telephone.Name = "text_Telephone";
+            this.text_Telephone.Size = new System.Drawing.Size(100, 25);
+            this.text_Telephone.TabIndex = 8;
+            // 
+            // text_Name
+            // 
+            this.text_Name.Location = new System.Drawing.Point(122, 24);
+            this.text_Name.Name = "text_Name";
+            this.text_Name.Size = new System.Drawing.Size(100, 25);
+            this.text_Name.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(359, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "地址：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(359, 63);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "年龄：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(359, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "单位：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "电话：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "性别：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "姓名：";
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Location = new System.Drawing.Point(167, 0);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(37, 15);
+            this.lbl_status.TabIndex = 0;
+            this.lbl_status.Text = "添加";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lv_Customer);
+            this.groupBox2.Location = new System.Drawing.Point(64, 282);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(640, 341);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "客户信息列表";
+            // 
+            // lv_Customer
+            // 
+            this.lv_Customer.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.编号,
+            this.姓名,
+            this.单位,
+            this.性别,
+            this.年龄,
+            this.电话,
+            this.地址});
+            this.lv_Customer.FullRowSelect = true;
+            this.lv_Customer.GridLines = true;
+            this.lv_Customer.HideSelection = false;
+            this.lv_Customer.Location = new System.Drawing.Point(0, 24);
+            this.lv_Customer.MultiSelect = false;
+            this.lv_Customer.Name = "lv_Customer";
+            this.lv_Customer.Size = new System.Drawing.Size(627, 300);
+            this.lv_Customer.TabIndex = 0;
+            this.lv_Customer.UseCompatibleStateImageBehavior = false;
+            this.lv_Customer.View = System.Windows.Forms.View.Details;
+            this.lv_Customer.SelectedIndexChanged += new System.EventHandler(this.lv_Customer_SelectedIndexChanged);
+            // 
+            // 编号
+            // 
+            this.编号.Text = "编号";
+            // 
+            // 姓名
+            // 
+            this.姓名.Text = "姓名";
+            // 
+            // 单位
+            // 
+            this.单位.Text = "单位";
+            // 
+            // 性别
+            // 
+            this.性别.Text = "性别";
+            // 
+            // 年龄
+            // 
+            this.年龄.Text = "年龄";
+            // 
+            // 电话
+            // 
+            this.电话.Text = "电话";
+            // 
+            // 地址
+            // 
+            this.地址.Text = "地址";
+            // 
+            // btn_Del
+            // 
+            this.btn_Del.Location = new System.Drawing.Point(542, 253);
+            this.btn_Del.Name = "btn_Del";
+            this.btn_Del.Size = new System.Drawing.Size(75, 23);
+            this.btn_Del.TabIndex = 2;
+            this.btn_Del.Text = "删除";
+            this.btn_Del.UseVisualStyleBackColor = true;
+            this.btn_Del.Click += new System.EventHandler(this.btn_Del_Click);
+            // 
+            // lbl_Note
+            // 
+            this.lbl_Note.AutoSize = true;
+            this.lbl_Note.Location = new System.Drawing.Point(231, 261);
+            this.lbl_Note.Name = "lbl_Note";
+            this.lbl_Note.Size = new System.Drawing.Size(55, 15);
+            this.lbl_Note.TabIndex = 3;
+            this.lbl_Note.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 662);
+            this.Controls.Add(this.lbl_Note);
+            this.Controls.Add(this.btn_Del);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudown_Age)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown nudown_Age;
+        private System.Windows.Forms.RadioButton rbtn_Sex2;
+        private System.Windows.Forms.RadioButton rbtn_Sex1;
+        private System.Windows.Forms.TextBox text_Adress;
+        private System.Windows.Forms.TextBox text_Company;
+        private System.Windows.Forms.TextBox text_Telephone;
+        private System.Windows.Forms.TextBox text_Name;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.ListView lv_Customer;
+        private System.Windows.Forms.Button btn_Del;
+        private System.Windows.Forms.Label lbl_Note;
+        private System.Windows.Forms.ColumnHeader 编号;
+        private System.Windows.Forms.ColumnHeader 姓名;
+        private System.Windows.Forms.ColumnHeader 单位;
+        private System.Windows.Forms.ColumnHeader 性别;
+        private System.Windows.Forms.ColumnHeader 年龄;
+        private System.Windows.Forms.ColumnHeader 电话;
+        private System.Windows.Forms.ColumnHeader 地址;
+    }
+}
+
---
+++ 新用户管理系统/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Net;
+
+namespace 新用户管理系统
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //零、清空文本框
+        protected void ClearTextbox()
+        {
+            text_Name.Text="";
+            text_Company.Text="";
+            text_Telephone.Text="";
+            text_Adress.Text="";
+            rbtn_Sex1.Checked=true;
+            nudown_Age.Value=1;
+            lbl_status.Text="添加";
+            customerid = "";
+        }
+
+        //一、添加客户信息
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            string name = text_Name.Text.Trim();
+            string company = text_Company.Text.Trim();
+            string telephone = text_Telephone.Text.Trim();
+            string address = text_Adress.Text.Trim();
+            string sex = rbtn_Sex1.Checked ? "男" : "女";
+            string age = nudown_Age.Value.ToString();
+
+            if (name == " ")
+            {
+                lbl_Note.Text = "姓名不能为空";
+                lbl_Note.ForeColor = Color.Red;
+                text_Name.Focus();
+            }
+            else if (lbl_status.Text == "添加")
+            {
+                SqlConnection con = new SqlConnection("Data Source=juzai66;database=wang;Integrated Security=SSPI");
+                con.Open();
+                string str = string.Format("insert into wmc2 values('{0}','{1}','{2}','{3}','{4}','{5}')", name, company, sex, age, telephone, address);
+                SqlCommand cmd = new SqlCommand(str, con);
+                int i = cmd.ExecuteNonQuery();
+                con.Close();
+                if (i > 0)
+                {
+                    lbl_Note.Text = "添加成功";
+                    lbl_Note.ForeColor = Color.Blue;
+                    ClearTextbox();
+                    DataBind_Customer();
+                }
+                else
+                {
+                    lbl_Note.Text = "添加失败";
+                    lbl_Note.ForeColor = Color.Red;
+                }
+
+            }
+            else    //修改操作
+            {
+                //1、创建数据库连接
+                SqlConnection comm = new SqlConnection("Data Source=juzai66; database=wang; Integrated Security=SSPI");
+                //2、打开连接
+                comm.Open();
+                //3、数据修改语句
+                string str = string.Format("update wmc2 set CustomerName='{0}',Company='{1}',Sex='{2}',Age={3},Telephone='{4}',Address='{5}' Where CustomerID={6}", name, company, sex, age, telephone, address, customerid);
+                //4、创建执行对象
+                SqlCommand cmd = new SqlCommand(str, comm);
+                //5、执行操作，返回受影响的行数
+                int i = cmd.ExecuteNonQuery();
+                //6、关闭连接
+                comm.Close();
+                //7、处理结果
+                if (i > 0)
+                {
+                    lbl_Note.Text = "恭喜您，客户信息修改成功！";
+                    lbl_Note.ForeColor = Color.Blue;
+                    ClearTextbox();    //调用函数，清空各控件
+                    DataBind_Customer();    //重新加载客户信息
+                }
+                else
+                {
+                    lbl_Note.Text = "对不起，客户信息修改失败！";
+                    lbl_Note.ForeColor = Color.Red;
+                }
+            }
+        }
+        //二、加载客户信息
+        protected void DataBind_Customer()
+        {
+            //1、创建并打开数据库连接
+            SqlConnection conn = new SqlConnection("Data Source=juzai66;database=wang;Integrated Security=SSPI");
+            conn.Open();
+            //2、客户端发出请求：数据查询语句
+            string str = "select * from wmc2";
+            //3、创建执行对象
+            SqlDataAdapter da = new SqlDataAdapter(str, conn);
+            //4、创建临时数据表
+            DataTable dt = new DataTable();
+            //5、执行查询，返回结果，填充到临时表中
+            da.Fill(dt);
+            //6、关闭连接
+            conn.Close();
+            //7、显示结果
+            lv_Customer.Items.Clear(); //先清空列表框视图控件中现有行
+            foreach (DataRow dr in dt.Rows)
+            {
+                ListViewItem myitem = new ListViewItem(dr["Customerid"].ToString()); //创建一个ListViewItem项，并为第1列赋值，客户编号
+                myitem.SubItems.Add(dr["name"].ToString()); //第2列，姓名
+                myitem.SubItems.Add(dr["company"].ToString()); //第3列，单位
+                myitem.SubItems.Add(dr["sex"].ToString()); //第4列，性别
+                myitem.SubItems.Add(dr["age"].ToString()); //第5列，年龄
+                myitem.SubItems.Add(dr["telephone"].ToString()); //第6列，电话
+                myitem.SubItems.Add(dr["address"].ToString()); //第7列，地址
+                lv_Customer.Items.Add(myitem); //将新建项添加到ListView控件中
+            }
+        }
+
+        //三、窗体加载事件
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataBind_Customer(); //加载客户信息
+        }
+
+        string customerid = "";  // 定义全局变量，用于存储客户编号
+
+
+
+        // 五、删除客户信息
+        private void btn_Del_Click(object sender, EventArgs e)
+        {
+            if (customerid == "")    //如果没有选中要删除的客户信息
+            {
+                MessageBox.Show("请先选择要删除的客户信息！");
+            }
+            else
+            {
+                //弹出删除确认提示框
+                DialogResult result = MessageBox.Show("确定要删除选中的客户信息？", "删除提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)    //如果确定删除
+                {
+                    //1、创建数据库连接
+                    SqlConnection comm = new SqlConnection("Data Source=juzai66; database=wang; Integrated Security=SSPI");
+                    //2、打开连接
+                    comm.Open();
+                    //3、数据修改语句
+                    string str = string.Format("delete from wmc2 where Customerid={0}", customerid);
+                    //4、创建执行对象
+                    SqlCommand cmd = new SqlCommand(str, comm);            //5、执行操作，返回受影响的行数
+                    int i = cmd.ExecuteNonQuery();
+                    //6、关闭连接
+                    comm.Close();
+                    //7、处理结果
+                    if (i > 0)
+                    {
+                        lbl_Note.Text = "恭喜您，客户信息删除成功！";
+                        lbl_Note.ForeColor = Color.Blue;
+                        ClearTextbox();    //调用函数，清空各控件
+                        DataBind_Customer();    //重新加载客户信息
+                    }
+                    else
+                    {
+                        lbl_Note.Text = "对不起，客户信息删除失败！";
+                        lbl_Note.ForeColor = Color.Red;
+                    }
+                }
+            }
+        }
+
+
+
+        //四、【客户信息】选中事件
+        private void lv_Customer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lv_Customer.SelectedItems.Count > 0)  // 如果有选中项
+            {
+                ListViewItem myitem = lv_Customer.SelectedItems[0];  // 获取选中的第一行（一次只能选一行）
+
+                customerid = myitem.SubItems[0].Text;  // 将选中行第1列的值赋值给全局变量，客户编号
+                text_Name.Text = myitem.SubItems[1].Text;  // 选中行第2列，姓名
+                text_Company.Text = myitem.SubItems[2].Text;  // 选中行第3列，单位
+                rbtn_Sex1.Checked = myitem.SubItems[3].Text == "男" ? true : false;  // 性别
+                rbtn_Sex2.Checked = myitem.SubItems[3].Text == "女" ? true : false;  // 性别
+                nudown_Age.Value = decimal.Parse(myitem.SubItems[4].Text);  // 年龄
+                text_Telephone.Text = myitem.SubItems[5].Text;  // 电话
+                text_Adress.Text = myitem.SubItems[6].Text;  // 地址
+                lbl_status.Text = "修改";  // 当前状态
+            }
+        }
+
+        //六、取消事件
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            ClearTextbox();    //调用函数，清空各控件
+            lbl_Note.Text = "";
+        }
+    }
+}
+
